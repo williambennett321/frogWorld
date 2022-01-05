4fc9d52b23decf657d9f151bcbddd7f08afdc937
williambennett321 <william.bennett321@outlook.com>
2022-01-05 01:53:31 -0500

    rendering All frogs page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Frog(models.Model):
+  name = models.CharField(max_length=100)
+  color_pat = models.CharField(max_length=100)
+  fun_fact = models.TextField(max_length=250)
+  lifespan = models.IntegerField()
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from .models import Frog
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Welcome</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def frogs_index(request):
+  frogs = Frog.objects.all()
   return render(request, 'frogs/index.html', { 'frogs': frogs })
